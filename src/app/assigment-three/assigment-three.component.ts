f6b325db127cfa7f26e3fce7e4857094042d2462
Freddy Polania <fpolania@gmail.com>
2019-12-13 18:10:39 -0400

    Assigment 3

--- src/app/assigment-three/assigment-three.component.ts
+++ src/app/assigment-three/assigment-three.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-assigment-three',
-  templateUrl: './assigment-three.component.html',
-  styleUrls: ['./assigment-three.component.css']
+  selector: "app-assigment-three",
+  templateUrl: "./assigment-three.component.html",
+  styleUrls: ["./assigment-three.component.css"]
 })
 export class AssigmentThreeComponent implements OnInit {
+  constructor() {}
 
-  paragraphVisible = false;
-  
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onDisplayMessage() {
-    this.paragraphVisible = true;
-    const el = document.querySelector('#Message');
-    console.log('clicked!');
-    
+    const el = document.querySelector("#Message");
+    if ((<HTMLElement>el).style.display === "none") {
+      (<HTMLElement>el).style.display = "block";
+    } else {
+      (<HTMLElement>el).style.display = "none";
+    }
+    console.log("clicked!");
   }
 }
