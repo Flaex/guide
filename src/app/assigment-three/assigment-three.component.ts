99792181aaadc1fb4380b15469059cb1d429e785
Freddy Polania <fpolania@gmail.com>
2020-01-10 18:40:01 -0400

    assigment-3 75%

--- src/app/assigment-three/assigment-three.component.ts
+++ src/app/assigment-three/assigment-three.component.ts
@@ -6,17 +6,37 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./assigment-three.component.css"]
 })
 export class AssigmentThreeComponent implements OnInit {
+  click = 0;
+  clickArray = [];
   constructor() {}
 
   ngOnInit() {}
 
   onDisplayMessage() {
-    const el = document.querySelector("#Message");
-    if ((<HTMLElement>el).style.display === "none") {
-      (<HTMLElement>el).style.display = "block";
+    const msgContainer = document.querySelector("#Message");
+    const clickCount = document.querySelector("#ClickCount");
+
+    if ((<HTMLElement>msgContainer).style.display === "none") {
+      (<HTMLElement>msgContainer).style.display = "block";
     } else {
-      (<HTMLElement>el).style.display = "none";
+      (<HTMLElement>msgContainer).style.display = "none";
     }
-    console.log("clicked!");
+
+    this.click++;
+    this.clickArray.push(this.click);
+
+    if (this.clickArray.length <= 4) {
+      clickCount.insertAdjacentHTML(
+        "beforeend",
+        `<div style="color:gray">${this.click}</div>`
+      );
+    } else {
+      clickCount.insertAdjacentHTML(
+        "beforeend",
+        `<div style="color:green">${this.click}</div>`
+      );
+    }
+    
+    console.log(this.clickArray);
   }
 }
